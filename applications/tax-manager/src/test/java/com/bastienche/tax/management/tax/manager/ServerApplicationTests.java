509e84313572af07190df5ae6b6022f9d01aead8
Bastien Chevriaut <bastien.chevriaut-ext@pole-emploi.fr>
2023-12-11 11:52:31 +0100

    init project

---
+++ applications/tax-manager/src/test/java/com/bastienche/tax/management/tax/manager/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bastienche.tax.management.tax.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
